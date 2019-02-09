8a7b577ebb6e151dc9b32192738d24a9d3777a18
Allen F Partlow <allenfp@gmail.com>
2019-02-09 05:20:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+word_subs = {
+          "hello" => 'hi',
+          "to, two, too" => '2',
+          "for, four" => '4',
+          'be' => 'b',
+          'you' => 'u',
+          "at" => "@" ,
+          "and" => "&",
+              }
+
+def word_substituter(strings)
+  x = strings.split(" ")
+  string_array = []
+  
+  x.each do |word|
+    if word_subs.keys.include? word
+      string_array.push(word_subs[word])
+    else
+      string_array.push(word)
+      
+  end
+  
+end

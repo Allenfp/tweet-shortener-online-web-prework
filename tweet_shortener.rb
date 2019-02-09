3c6790a6ed31f577301b6d674219170744c82423
Allen F Partlow <allenfp@gmail.com>
2019-02-09 05:43:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -95,3 +95,41 @@ def selective_tweet_shortener(tweet)
       tweet
     end
 end
+
+
+def shortened_tweet_truncator(tweet)
+
+      word_subs = {
+                "hello" => 'hi',
+                "to" => '2',
+                "two" => '2',
+                "too" => '2',
+                "for"=> '4',
+                "four" => '4',
+                'be' => 'b',
+                'you' => 'u',
+                "at" => "@" ,
+                "and" => "&",
+                    }
+
+      if tweet.length > 140
+
+
+          x = tweet.split(" ")
+          string_array = []
+
+          x.each do |word|
+            if word_subs.keys.include? word.downcase
+              string_array.push(word_subs[word.downcase])
+            else
+              string_array.push(word)
+            end
+          end
+          return_string = string_array.join(" ")
+          return return_string[0:140]
+
+      else
+        tweet
+      end
+
+end

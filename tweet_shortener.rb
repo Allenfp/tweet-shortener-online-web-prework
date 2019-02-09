068e0c4d306e4a9273886debfbe396b1a65fa62b
Allen F Partlow <allenfp@gmail.com>
2019-02-09 05:23:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,21 @@
 
-word_subs = {
-          "hello" => 'hi',
-          "to, two, too" => '2',
-          "for, four" => '4',
-          'be' => 'b',
-          'you' => 'u',
-          "at" => "@" ,
-          "and" => "&",
-              }
 
 def word_substituter(strings)
+
+  
+  word_subs = {
+            "hello" => 'hi',
+            "to, two, too" => '2',
+            "for, four" => '4',
+            'be' => 'b',
+            'you' => 'u',
+            "at" => "@" ,
+            "and" => "&",
+                }
+
+
+
+  
   x = strings.split(" ")
   string_array = []
 
